3c2bbd2de135da93bdcf6eaaa6c944f81cda549c
jmvera255 <56414911+jmvera255@users.noreply.github.com>
2022-12-21 19:10:59 -0600

    increase smoothing

--- code/VNS.m
+++ code/VNS.m
@@ -11,7 +11,7 @@ toc
 
 tic
 disp('Performing smoothing and contrast adjustment')
-Img1_smooth = imgaussfilt(Img1,1); %smooth image
+Img1_smooth = imgaussfilt(Img1,2); %smooth image
 %he = imadjust(Img1_smooth, [.2 .3 0; .6 .7 1],[]); %default adjust contrast in image
 %he = imadjust(Img1_smooth, [0.1 .3 .1; .5 .7 .9],[]);
 %he = imadjust(Img1_smooth, [0.1 .3 .1; .85 .75 .9],[]);
